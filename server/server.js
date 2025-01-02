c9dc799d34dd5eb03a6abeb99a5021c0f034d41a
Jim-299 <jinnocent691@gmail.com>
2025-01-02 08:55:14 +0300

    Can request posts

--- server/server.js
+++ server/server.js
@@ -14,6 +14,28 @@ app.get('/', (req, res) => {
     res.json({ message: "Backend is active" });
 });
 
+app.get('/posts', async (req, res) => {
+    try {
+        const response = await fetch('https://marcmedics.com/wp-json/wp/v2/posts?per_page=100', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            console.error("Error getting posts from WordPress:", await response.json());
+            return res.status(response.status).json({ error: 'Failed to fetch posts from WordPress' });
+        }
+
+        const data = await response.json(); // Parse JSON data
+        res.json(data); // Send the data to the client
+    } catch (error) {
+        console.error("Error making a request to WordPress:", error);
+        res.status(500).json({ error: 'An error occurred while fetching posts' });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
